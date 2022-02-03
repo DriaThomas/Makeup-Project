0af596896ee96c173d96c7d2e82784e8837325d6
Dria Thomas <driathomas96@gmail.com>
2022-02-02 21:27:20 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ router.get("/", async (req, res) => {
     );
 
     let data = await Promise.all(trimmedArrOfProductsAndRevLength);
-    // console.log("homepage", productsFromApi);
+    console.log("productaapi");
     res.render("index.hbs", {
       productsFromApi: data,
       isLoggedIn: req.session.user,
